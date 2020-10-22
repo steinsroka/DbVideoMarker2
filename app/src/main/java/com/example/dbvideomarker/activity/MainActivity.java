7f70dc394d52b60fe0cc36a9f47ad0ac9e8f56ac
con159 <65004116+con159@users.noreply.github.com>
2020-10-22 09:09:18 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/dbvideomarker/activity/MainActivity.java
+++ app/src/main/java/com/example/dbvideomarker/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.dbvideomarker.R;
+import com.example.dbvideomarker.adapter.VideoAdapter;
 import com.example.dbvideomarker.adapter.ViewPagerAdapter;
 import com.example.dbvideomarker.ui.mark.MarkFragment;
 import com.example.dbvideomarker.ui.home.HomeFragment;
@@ -15,10 +16,12 @@ import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static ViewPagerAdapter adapter;
     MarkFragment markFragment;
     HomeFragment homeFragment;
     PlaylistFragment playlistFragment;
@@ -28,36 +31,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
+    private void initViews() {
         toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar); //툴바를 액션바(앱바)로 사용할 수 있는 메소드
+        setSupportActionBar(toolbar);
 
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setOffscreenPageLimit(3);
-//        setupViewPager(viewPager);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        setupViewPager(viewPager);//Set up View Pager
 
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);//setting tab over viewpager
         tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#D5355F"));
         tabLayout.setTabTextColors(Color.parseColor("#73D5355F"), Color.parseColor("#D5355F"));
-
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home,
-//                R.id.navigation_dashboard,
-//                R.id.navigation_notifications).build();
-//
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
-
-        setupViewPager(viewPager);
     }
 
     private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
         markFragment = new MarkFragment();
         homeFragment = new HomeFragment();
         playlistFragment = new PlaylistFragment();
@@ -67,32 +58,13 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
-    public void disableToolbar() {
-        toolbar.setVisibility(View.GONE);
+    public Fragment getFragment(int pos) {
+        return adapter.getItem(pos);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar, menu);
-//        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-
-//        searchView.setMaxWidth(Integer.MAX_VALUE);
-//        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
-//
-//        searchView.setQueryHint("검색어를 입력하세요");
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//                Toast.makeText(getApplicationContext(), "검색을 완료했습니다.", Toast.LENGTH_SHORT).show();
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                Toast.makeText(getApplicationContext(), "입력중입니다.", Toast.LENGTH_SHORT).show();
-//                return false;
-//            }
-//        });
         return true;
     }
 
