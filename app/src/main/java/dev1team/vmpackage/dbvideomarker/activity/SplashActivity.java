d6dd370659941a423557d3937377cd4c4841e8ca
con159 <65004116+con159@users.noreply.github.com>
2020-11-25 17:11:04 +0900

    수정

--- app/src/main/java/dev1team/vmpackage/dbvideomarker/activity/SplashActivity.java
+++ app/src/main/java/dev1team/vmpackage/dbvideomarker/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package dev1team.vmpackage.dbvideomarker.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +13,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        startLoading();
+    }
 
-        finish();
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(getApplicationContext(),  MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
